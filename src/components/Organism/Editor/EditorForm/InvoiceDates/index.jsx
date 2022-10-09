3177a0063d115fbf9edd3e70db36582c329c5a30
Remlad <isiakaabd@gmail.com>
2022-10-09 13:51:09 +0100

    resolving issue #31- The invoice due date should be after the invoice date

--- src/components/Organism/Editor/EditorForm/InvoiceDates/index.jsx
+++ src/components/Organism/Editor/EditorForm/InvoiceDates/index.jsx
@@ -6,6 +6,7 @@ import {
   FormLabel,
   Input,
   Stack,
+  useToast,
   useColorModeValue,
 } from '@chakra-ui/react';
 
@@ -15,7 +16,7 @@ export default function InvoiceDates({ invoiceDates, getDates, resetForm }) {
   const formik = useFormik({
     initialValues: { invoiceDates },
   });
-
+  const toast = useToast();
   useEffect(() => {
     getDates(formik.values.invoiceDates);
     if (resetForm) {
@@ -23,6 +24,30 @@ export default function InvoiceDates({ invoiceDates, getDates, resetForm }) {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formik.values]);
+
+  const handleDateChange = e => {
+    if (formik.values.invoiceDates[1] !== '') {
+      formik.setFieldValue('invoiceDates[2]', e.target.value);
+    } else {
+      toast({
+        status: 'error',
+        title: 'Select an invoice Date',
+        duration: 2000,
+        isClosable: true,
+        position: 'bottom-right',
+      });
+
+      formik.setFieldValue('invoiceDates[2]', '');
+    }
+  };
+
+  // suppose the invoice date is cleared or changed  and the new invoice date selected is ahead of invoice due date, the useEffect runs
+  useEffect(() => {
+    if (formik.values.invoiceDates[1] > formik.values.invoiceDates[2]) {
+      formik.setFieldValue('invoiceDates[2]', '');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formik.values.invoiceDates[1], formik.values.invoiceDates[2]]);
 
   return (
     <>
@@ -71,8 +96,14 @@ export default function InvoiceDates({ invoiceDates, getDates, resetForm }) {
               bg={useColorModeValue('gray.100', 'gray.700') || 'gray.200'}
               color={useColorModeValue('gray.800', 'gray.300') || 'gray.800'}
               name="invoiceDates[2]"
-              onChange={formik.handleChange}
-              value={formik.values.invoiceDates[2]}
+              onChange={handleDateChange}
+              min={formik.values.invoiceDates[1]}
+              value={
+                formik.values.invoiceDates[1] &&
+                formik.values.invoiceDates[1] < formik.values.invoiceDates[2]
+                  ? formik.values.invoiceDates[2]
+                  : ''
+              }
             />
           </FormControl>
         </Box>
